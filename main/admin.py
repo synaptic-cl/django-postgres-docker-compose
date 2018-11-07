442001bb849f5ae994dc25fa2e245bba3b9f32f6
Sam Hernández <sam@mispistachos.cl>
2018-11-07 12:39:31 -0300

    add: filters (list_filter), custom field displaying in table (list_display) + custom form distribution (fieldsets) + custom actions (actions + account_actions)

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,54 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Product, Family, Location#, Transaction
+
+admin.site.register(Family)
+admin.site.register(Location)
+# admin.site.register(Transaction)
+
+admin.site.site_header = 'Tiendapp header'
+admin.site.site_title = 'Tiendapp title'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all": ("my_styles.css"),
+        }
+        js = ("my_code.js",)
+    list_display = ['sku', 'title', 'unit', 'unitCost', 'quantity', 'created_at', 'account_actions']
+    list_filter = ('sku', 'title', 'unit', 'unitCost', 'quantity', 'created_at', 'updated_at')
+    # fields = [('family','location'),('sku','barcode'), ('title','description'),('unit', 'unitCost'), ('quantity','minQuantity')]
+    fieldsets = (
+        ('Section 1', {
+            'fields': ('family','location')
+        }),
+        ('Section 2', {
+            'fields': ('title','description')
+        }),        
+        ('Section 3', {
+            'fields': ('sku','barcode','unit', 'unitCost','quantity','minQuantity')
+        }),
+    )
+    actions = ['deposit', 'withdraw']
+
+    def account_actions(self, obj):
+        #return render_a_template('alksdjkl/qwhd/actions.html')
+        return format_html(
+            '<a class="button" href="">Deposit</a>&nbsp<a class="button" href="">Withdraw</a>'
+        )
+    account_actions.short_description = 'Account Actions'
+    account_actions.allow_tags = True
+
+
+class CSSAdminMixin(object):
+    class Media:
+        css = {
+            'all': ('dashboard_solo.css'),
+        }
+
+
+admin.site.register(Product, ProductAdmin)
